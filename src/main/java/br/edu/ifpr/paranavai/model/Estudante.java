66cf1dc7b44f169e2534e27086142c13d672d437
Leonardo <leonardosarto1@hotmail.com>
2022-09-21 23:15:14 -0300

    Criação do package dao

--- src/main/java/br/edu/ifpr/paranavai/model/Estudante.java
+++ src/main/java/br/edu/ifpr/paranavai/model/Estudante.java
@@ -1,5 +1,21 @@
 package br.edu.ifpr.paranavai.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "estudante")
 public class Estudante extends Pessoa{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "estudante_id", unique = true, nullable = false)
+    private Long id;
+    
+    @Column(name="ra")
     private String ra;
 }
--- src/main/java/br/edu/ifpr/paranavai/model/Reserva.java
+++ src/main/java/br/edu/ifpr/paranavai/model/Reserva.java
@@ -1,6 +1,21 @@
 package br.edu.ifpr.paranavai.model;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "reserva")
 public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "bibliotecario_id", unique = true, nullable = false)
+    private Long id;
+    
     private Pessoa pessoa;
     private Armario armario;
     private Date dataHoraDevolucao;
